766df6f12deccc6c3d66772c8fb3123eeb14df61
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-19 14:48:41 +0100

    Component with props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import DisplayMyName from './DisplayName'
+import DisplayNames from './DisplayNames'
 
 const App = () => (
   <div>
     <h1>Hello World!</h1>
     <DisplayMyName/>
+    <DisplayNames
+      name='Krystian'
+    />
   </div>
 )
 
---
+++ src/DisplayNames.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DisplayName = (props) => {
+    console.log(props)
+    return (
+    <div>
+        {props.name}
+    </div>
+    )
+}
+
+
+export default DisplayName
